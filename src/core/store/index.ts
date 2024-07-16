12c5122c294b5ca183b3eaf36b953eb7d662b61b
Antonio Pires <tonan@outlook.com.br>
2024-07-16 07:36:54 -0300

    Reacting to Thunks

---
+++ src/core/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {postReducer} from "./Post.slice";
+
+const store = configureStore({
+    reducer: {
+        post: postReducer,
+    },
+});
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
