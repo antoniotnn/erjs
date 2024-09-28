58c46758a51ea9fd4d34e9d3ede5d77edbae5ce3
Antonio Pires <tonan@outlook.com.br>
2024-09-28 08:20:06 -0300

    The first unit test

--- src/tests/CircleChart.test.tsx
+++ src/tests/CircleChart.test.tsx
@@ -1,7 +1,17 @@
 import '@testing-library/react'
+import {render, screen} from "@testing-library/react";
+import CircleChart from "../app/components/CircleChart";
 
 
-test('true é truthy', () => {
-    const value = true;
-    expect(value).toBeTruthy();
+it('renders caption', () => {
+    render(
+        <CircleChart
+            size={180}
+            progress={80}
+            caption={'javascript'}
+        />
+    );
+
+    const caption = screen.getByText('javascript');
+    expect(caption).toBeVisible();
 });
