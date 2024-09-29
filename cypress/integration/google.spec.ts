03a234be22f436c656e6ab2d400b1403f0722df8
Antonio Pires <tonan@outlook.com.br>
2024-09-29 10:38:34 -0300

    Automating tasks on pages

--- cypress/integration/google.spec.ts
+++ cypress/integration/google.spec.ts
@@ -2,9 +2,17 @@
 /* eslint-disable */
 // @ts-ignore
 
-describe('My first test', () => {
-  it('compares value with true', () => {
-    const value = true;
-    expect(value).to.equal(true);
-  })
+describe("My first test", () => {
+    it("compares value with true", () => {
+        const value = true;
+        expect(value).to.equal(true);
+    });
+
+    it('searches for "teste automatizado com cypress" in google', () => {
+        cy.visit("https://google.com.br");
+
+        cy.get('input[type="text"]').type("teste automatizado com cypress");
+        cy.contains("Pesquisa Google").click();
+    });
+
 });
