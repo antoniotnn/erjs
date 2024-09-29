47a57080a330ee1790691439ac8f8dbda7df1178
Antonio Pires <tonan@outlook.com.br>
2024-09-29 10:24:37 -0300

    Installing and configuring Cypress

---
+++ cypress/integration/google.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+/* eslint-disable */
+// @ts-ignore
+
+describe('My first test', () => {
+  it('compares value with true', () => {
+    const value = true;
+    expect(value).to.equal(true);
+  })
+});
